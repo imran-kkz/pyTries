1fb7d1ed8434f81a533ad6eaad2c4158f8e57826
imran-kkz <imranrali1@gmail.com>
2020-07-16 01:39:56 -0400

    strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.8
+
+stri = 'single quoted string'
+
+print(stri)
+
+str2 = "double quoted string"
+
+print (str2)
+
+print (str2*2)
+
+funcStr = str2.find('z')
+
+print (funcStr)
+
+print ("teSTinG".lower())
