29abd41d15cba299676c42b5bbb226c2f259c4f6
imran-kkz <imranrali1@gmail.com>
2020-07-17 23:21:12 -0400

    add number systems and floating point accuracy

---
+++ numbers.py
@@ -0,0 +1,37 @@
+print (2+2)
+
+print (2.0 + 2.0)
+
+print (4.5e9)
+
+print (4.5e-2)
+
+#Operators
+
+print (2+2) #Addition
+
+print (2-2) #Subtraction
+
+print (2*2) #Multiplication
+
+print (5/3) #Division
+
+print (5//3) #Floor Division, always returns a number without the remainder as an int
+
+print (8%3) #Modulo, returns the remainder as an int
+
+print (2**3) #Exponent
+
+#Number Systems
+
+print ("=======Number Systems========")
+
+# Decimal systems = base 10
+# Binary = Base 2
+# Octal = Base 8
+# Hexadecimal = base 16
+
+# Floating point accuracy
+# not all decimals be represented as binary fractions
+# example: 0.1 can't be represented cleanly
+
