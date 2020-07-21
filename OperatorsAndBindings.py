a073f4d037be93aeb2afc569b279adf50d887d0b
imran-kkz <imranrali1@gmail.com>
2020-07-20 23:22:24 -0400

    add comparisionOperators.py

---
+++ OperatorsAndBindings.py
@@ -0,0 +1,24 @@
+# Unary and Bitwise Operators
+
+a = 0b010
+
+print (a)
+
+# ~ two's compliment
+# it takes a number that we're going to call x, and returns the result of -x - 1
+
+print (~a)
+
+# Main ones to know:
+
+#AND OR XOR NOT
+
+b = 0b1001
+c = 0b1100
+
+print (bin(b | c)) # OR
+
+print (bin(b & c)) # AND
+
+print (bin(b ^ c)) # XOR (exclusive OR)
+
---
+++ comparisionOperators.py
@@ -0,0 +1,7 @@
+print (1>2) #prints out bool of nums in brackets
+
+print (1<2) #True
+
+print ('a' > 'b') #False
+
+print ('b' > 'a') #True
