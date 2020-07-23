b16625b841dc0820ee1cae26c0dab1223d16cc9f
imran-kkz <imranrali1@gmail.com>
2020-07-22 23:54:55 -0400

    args for print

--- inputs.py
+++ inputs.py
@@ -1,7 +1,10 @@
 # learning inputs in python
 
 name = input("What is your name?")
-favorite = input("What is your fav colour?")
+fav = input("What is your fav colour?")
 age = int(input("How old are you?"))
 
-print("Your favorite colour is: " + favorite)
+print("Your name is " + name + " and your favourite colour is " + fav, end=" ")
+print(str(age))
+
+print (name, 'is', age, 'years old and loves the color', fav + ".", sep= " ")
