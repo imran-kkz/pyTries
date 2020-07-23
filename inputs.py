97e8d00503911052d9e21ab1a00f9219f5c6ea28
imran-kkz <imranrali1@gmail.com>
2020-07-22 23:43:15 -0400

    continuing with learning python

---
+++ inputs.py
@@ -0,0 +1,7 @@
+# learning inputs in python
+
+name = input("What is your name?")
+favorite = input("What is your fav colour?")
+age = int(input("How old are you?"))
+
+print("Your favorite colour is: " + favorite)
---
+++ typecasting.py
@@ -0,0 +1,17 @@
+# Learning Typecasting
+
+print (float(1))
+
+print (int(1.6))
+
+print (str(2.6))
+
+print (bool(1.0))
+
+print (bool (0))
+
+print ('This' and 'That')
+
+print ('This' and 0  and 'That')
+
+print (0 or 1)
